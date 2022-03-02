2f39a042acba6994e917f5945805d7ca499bc1ea
waneta <335146812@qq.com>
2022-03-02 15:17:35 +0800

    modify
    modify

---
+++ Runtime/RotateMe2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace upmlib
+{
+    public class RotateMe2 : MonoBehaviour
+    {
+        public float angularVelocity = 10;
+        void Update()
+        {
+            var rot = Quaternion.AngleAxis(angularVelocity * Time.deltaTime, Vector3.up);
+            transform.localRotation = rot * transform.localRotation;
+        }
+    }
+}
